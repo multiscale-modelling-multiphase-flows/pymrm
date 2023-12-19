7b6b4e4b210f1a3fc863f053792ce236387ba737
eajfpeters <e.a.j.f.peters@tue.nl>
2023-12-20 00:17:45 +0100

    Many updates including Jupyter notebook with examples

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_mrm',
+    version='1.2.0',
+    author='E.A.J.F. Peters',
+    author_email='e.a.j.f.peters@tue.nl',
+    description='Functions for multiphase reactor modeling',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+)
