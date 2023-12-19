c6cc520c532232246ae885f8385686ca5d16feab
eajfpeters <e.a.j.f.peters@tue.nl>
2023-12-20 00:51:14 +0100

    Upated setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     author='E.A.J.F. Peters',
     author_email='e.a.j.f.peters@tue.nl',
     description='Functions for multiphase reactor modeling',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'numpy',
