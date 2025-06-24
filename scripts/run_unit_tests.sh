21dc51d7b118e96e71389c4887eca010449fee42
eajfpeters <e.a.j.f.peters@tue.nl>
2025-06-24 22:57:02 +0200

    Fix Codecov action configuration: change 'path' to 'files' for coverage upload

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -5,4 +5,4 @@
 # execute from root folder!
 
 python -m pip install -e .
-pytest --cov=. --import-mode=importlib --cov-report=html --cov-report=xml:test/coverage.xml --cov-config=.coveragerc
+pytest --junitxml=test/pytest-results.xml --cov=. --import-mode=importlib --cov-report=html --cov-report=xml:test/coverage.xml --cov-config=.coveragerc
