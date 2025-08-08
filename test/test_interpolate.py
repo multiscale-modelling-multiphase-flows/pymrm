2034c7166bf4f614c66367a703f91c20b9bb7da7
Frank Peters <e.a.j.f.peters@tue.nl>
2025-08-08 17:17:02 +0200

    Create tests for compute_boundary_values and construct_boundary_value_matrices

--- test/test_interpolate.py
+++ test/test_interpolate.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from pymrm.interpolate import (
     interp_stagg_to_cntr, interp_cntr_to_stagg, interp_cntr_to_stagg_tvd,
-    create_staggered_array, compute_boundary_values
+    create_staggered_array, compute_boundary_values, construct_boundary_value_matrices
 )
 from pymrm.convect import upwind
 
@@ -36,9 +36,37 @@ def test_create_staggered_array():
     assert result.shape[0] == 11
 
 def test_compute_boundary_values():
-    arr = np.arange(10.0).reshape((10, 1, 1))
-    x_f = np.linspace(0, 1, 11)
-    x_c = np.linspace(0.05, 0.95, 10)
-    bc = ({'a': 1, 'b': 0, 'd': 0}, {'a': 1, 'b': 0, 'd': 0})
-    result = compute_boundary_values(arr, x_f, x_c=x_c, bc=bc, axis=0)
-    assert isinstance(result, tuple)
+    tol = 1e-12
+    num_x = 2
+    x_f = np.linspace(0.0, 1.0, num_x + 1)
+    c = 0.5 * (x_f[1:] + x_f[:-1]).copy()
+
+    # Left boundary condition
+    bc_left = {'a': -1, 'b': 1, 'd': 1}
+    boundary_value, boundary_grad = compute_boundary_values(c, x_f, bc=bc_left, bound_id=0)
+    assert abs(boundary_value) < tol
+    assert abs(boundary_grad - 1.0) < tol
+
+    # Right boundary condition
+    bc_right = {'a': 1, 'b': 1, 'd': 2}
+    boundary_value, boundary_grad = compute_boundary_values(c, x_f, bc=bc_right, bound_id=1)
+    assert abs(boundary_value - 1.0) < tol
+    assert abs(boundary_grad - 1.0) < tol
+
+def test_construct_boundary_value_matrices():
+    tol = 1e-12
+    num_x = 2
+    x_f = np.linspace(0.0, 1.0, num_x + 1)
+    c = 0.5 * (x_f[1:] + x_f[:-1]).copy()
+
+    # Left boundary condition
+    bc_left = {'a': -1, 'b': 1, 'd': 1}
+    matrix, matrix_bc = construct_boundary_value_matrices(c.shape, x_f, bc=bc_left, bound_id=0)
+    boundary_value = matrix @ c.reshape((-1, 1)) + matrix_bc
+    assert np.allclose(boundary_value, 0.0, atol=tol)
+
+    # Right boundary condition
+    bc_right = {'a': 1, 'b': 1, 'd': 2}
+    matrix, matrix_bc = construct_boundary_value_matrices(c.shape, x_f, bc=bc_right, bound_id=1)
+    boundary_value = matrix @ c.reshape((-1, 1)) + matrix_bc
+    assert np.allclose(boundary_value, 1.0, atol=tol)
